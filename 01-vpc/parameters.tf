e2605bbce32631198525442ccccc88b84d55f661
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2025-01-08 12:22:02 +0530

    Update parameters.tf

--- 01-vpc/parameters.tf
+++ 01-vpc/parameters.tf
@@ -1,12 +1,12 @@
 resource "aws_ssm_parameter" "vpc_id" {
-  name  = "/${var.project_name}/${var.environment}/vpc_id"
+  name  =  "/${var.project_name}/${var.environment}/prod_vpc_id"
   type  = "String"
   value = module.roboshop.vpc_id
 }
 
 # we will get tuple so we will convert into list by using join function vid 32 1:13:13
 resource "aws_ssm_parameter" "public_subnets_ids" {
-  name  = "/${var.project_name}/${var.environment}/public_subnets_ids"
+  name  = "/${var.project_name}/${var.environment}/prod_public_subnets_ids"
   type  = "StringList"
   value = join(",",module.roboshop.public_subnet_ids)
 }
@@ -24,13 +24,13 @@ output "private_subnets_ids" {
 # since these are stringList we are processing them to a list and 
 # uploading it into ssm parameters store (name of ssm parameter store variable is plural)
 resource "aws_ssm_parameter" "private_subnets_ids" {
-  name  = "/${var.project_name}/${var.environment}/private_subnets_ids"
+  name  = "/${var.project_name}/${var.environment}/prod_private_subnets_ids"
   type  = "StringList"
   value = join(",",module.roboshop.private_subnet_ids)
 }
 
 resource "aws_ssm_parameter" "database_subnets_ids" {
-  name  = "/${var.project_name}/${var.environment}/database_subnets_ids"
+  name  = "/${var.project_name}/${var.environment}/prod_database_subnets_ids"
   type  = "StringList"
   value = join(",",module.roboshop.database_subnet_ids)
 }
