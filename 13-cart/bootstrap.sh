6249cfa4ac6fa2935ba5fb37082127c4d5660274
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2025-01-10 17:43:30 +0530

    python upgrade to3.13

--- 13-cart/bootstrap.sh
+++ 13-cart/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 component=$1
 environment=$2 #dont use env here, it is reserved in linux
-yum install python3.11-devel python3.11-pip -y
+yum install python3.13-devel python3.13-pip -y
 pip3.11 install ansible botocore boto3
 ansible-pull -U https://github.com/PramodCodes/roboshop-ansible-roles-tf.git -e component=$component -e env=$environment main-tf.yaml
