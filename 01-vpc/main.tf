b5921c2ca3d1dd645679ae82eedcec29b549d8be
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2025-01-11 17:41:13 +0530

    Update main.tf

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -1,15 +1,4 @@
 module "roboshop" {
-  #source = "../terraform-aws-vpc"
-  # source = "git::https://github.com/daws-76s/terraform-aws-vpc.git?ref=main"
-  # project_name = var.project_name
-  # environment = var.environment
-  # common_tags = var.common_tags
-  # vpc_tags = var.vpc_tags
-  # vpc_cidr = var.vpc_cidr
-  # public_subnets_cidr = var.public_subnets_cidr
-  # private_subnets_cidr = var.private_subnets_cidr
-  # database_subnets_cidr = var.database_subnets_cidr
-  # is_peering_required = var.is_peering_required
     source = "git::https://github.com/Pramodcodes/tarraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     project_environment = var.project_environment
