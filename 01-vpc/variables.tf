3dbe29421a03d4d54e73aac19414504a476f9d3b
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2025-01-11 16:12:24 +0530

    Update variables.tf

--- 01-vpc/variables.tf
+++ 01-vpc/variables.tf
@@ -21,15 +21,15 @@ variable "environment" {
   default = "prod"
 }
 
-variable "public_subnets_cidr" {
-  default = ["10.1.1.0/24", "10.1.2.0/24"]
-}
-variable "private_subnets_cidr" {
-  default = ["10.1.11.0/24", "10.1.12.0/24"]
-}
-variable "database_subnets_cidr" {
-  default = ["10.1.21.0/24", "10.1.22.0/24"]
-}
+# variable "public_subnets_cidr" {
+#   default = ["10.1.1.0/24", "10.1.2.0/24"]
+# }
+# variable "private_subnets_cidr" {
+#   default = ["10.1.11.0/24", "10.1.12.0/24"]
+# }
+# variable "database_subnets_cidr" {
+#   default = ["10.1.21.0/24", "10.1.22.0/24"]
+# }
 variable "is_peering_required" {
   default = true
 }
@@ -45,6 +45,5 @@ variable "private_subnet_cidr" {
   default = ["10.1.11.0/24", "10.1.12.0/24"]
 }
 variable "database_subnet_cidr" {
-  default = ["10.1.21.0/24", "10.1.22.0/24"]
   default = ["10.1.21.0/24", "10.1.22.0/24"]
 }
